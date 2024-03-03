8147cbbb6dfb040f2a7b1e615d8e84d2a8c3d4e3
Julien Coolen <julien.coolen@nomadic-labs.com>
2024-03-03 16:51:25 +0100

    no flambda in nix flake+update broken link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 OCaml bindings to the [PARI](https://pari.math.u-bordeaux.fr/) library -- a cross platform and open-source computer algebra system designed for fast computations in number theory.
 
-The API is documented in the [Users' Guide to the PARI library](https://pari.math.u-bordeaux.fr/pub/pari/manuals/2.15.1/libpari.pdf).
+The API is documented in the [Users' Guide to the PARI library](https://pari.math.u-bordeaux.fr/pub/pari/manuals/2.16.1/libpari.pdf).
 
 The best way to get the dependencies is through [Nix](https://nixos.org/).
 Without Nix, you will need opam 2.0 and GMP installed on your system.
--- default.nix
+++ default.nix
@@ -3,9 +3,10 @@ let pkgs = import (fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/1f0e8ac1f9a783c4cfa0515483094eeff4315fe2.tar.gz";
   sha256 = "1mdnn0fj81pgvhzmzxh0g54g6yqxfqd2fim4h4c7cf7yskcp8g48";
 }) {inherit system; }; in
-let ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_0.overrideScope' (self: super: {
-  ocaml = super.ocaml.override { flambdaSupport = true; };
-});
+let ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_0;
+#.overrideScope' (self: super: {
+#  ocaml = super.ocaml.override { flambdaSupport = true; };
+#});
 in pkgs.fastStdenv.mkDerivation {
   name = "ocaml_pari";
   nativeBuildInputs =
